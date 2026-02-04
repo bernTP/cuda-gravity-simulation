f7a4c143728a1c9b88a01472c3234e9eb8714b43
berntp <thomas.pommier04@hotmail.fr>
2026-02-04 04:46:54 +0100

    feat: add steps_per_frame option for skipping insignificant changes + elapsed time

--- main.cu
+++ main.cu
@@ -33,10 +33,11 @@ int main(int argc, char **argv)
     int n = 1024;
     int block_size = 256;
     int iterations = 10;
+    int steps_per_frame = 20;
     std::string mode = "naive";
 
     int opt;
-    while ((opt = getopt(argc, argv, "n:b:i:m:")) != -1)
+    while ((opt = getopt(argc, argv, "n:s:b:i:m:")) != -1)
     {
         switch (opt)
         {
@@ -51,6 +52,9 @@ int main(int argc, char **argv)
             break;
         case 'm':
             mode = optarg;
+            break;
+        case 's':
+            steps_per_frame = std::stoi(optarg);
             break;
         default:
             std::cerr << "Usage: " << argv[0] << " -n <num_particles> -b <block_size> -i <iterations> -m <naive|tiled>\n";
@@ -77,23 +81,42 @@ int main(int argc, char **argv)
 
     int num_blocks = (n + block_size - 1) / block_size;
 
-    for (int iter = 0; iter < iterations; ++iter)
+    cudaEvent_t global_start, global_stop;
+    CUDA_CHECK(cudaEventCreate(&global_start));
+    CUDA_CHECK(cudaEventCreate(&global_stop));
+    CUDA_CHECK(cudaEventRecord(global_start));
+
+    for (int iter = 0; iter < iterations; iter += steps_per_frame)
     {
-        if (mode == "naive")
+        for (int s = 0; s < steps_per_frame; ++s)
         {
-            nbody_naive_kernel<<<num_blocks, block_size>>>(d_particles, n, dt, softening);
-        }
-        else
-        {
-            size_t shared_mem_size = block_size * sizeof(Particle);
-            nbody_tiled_kernel<<<num_blocks, block_size, shared_mem_size>>>(d_particles, n, dt, softening);
+            if (mode == "naive")
+            {
+                nbody_naive_kernel<<<num_blocks, block_size>>>(d_particles, n, dt, softening);
+            }
+            else
+            {
+                size_t shared_mem_size = block_size * sizeof(Particle);
+                nbody_tiled_kernel<<<num_blocks, block_size, shared_mem_size>>>(d_particles, n, dt, softening);
+            }
         }
 
         CUDA_CHECK(cudaGetLastError());
+
         CUDA_CHECK(cudaDeviceSynchronize());
         CUDA_CHECK(cudaMemcpy(h_particles.data(), d_particles, n * sizeof(Particle), cudaMemcpyDeviceToHost));
         print_csv_row(h_particles);
     }
+
+    CUDA_CHECK(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaEventRecord(global_stop));
+    CUDA_CHECK(cudaEventSynchronize(global_stop));
+    float elapsed_ms = 0.0f;
+    CUDA_CHECK(cudaEventElapsedTime(&elapsed_ms, global_start, global_stop));
+    std::cerr << "Total elapsed (s): " << (elapsed_ms / 1000.0f) << "\n";
+
+    CUDA_CHECK(cudaEventDestroy(global_start));
+    CUDA_CHECK(cudaEventDestroy(global_stop));
 
     CUDA_CHECK(cudaFree(d_particles));
     return 0;
